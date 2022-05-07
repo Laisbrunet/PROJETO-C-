beb5458696fd7904bed8db5047473b25fd1fe3d3
Laís Brunet Tomaz da Silva <laisbrunettomaz@gmail.com>
2022-05-07 18:46:21 -0300

    Página Inicial do C#
    HelloCsharp

--- Program.cs
+++ Program.cs
@@ -1,64 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Como criar uma lista de nomes
-var names = new List<string> { "<Lais>", "Ana", "Felipe" };
-foreach (var Lais in names)
-{
-  Console.WriteLine($"Hello {Lais.ToUpper()}!");
-}
-//tipo List<T> armazena sequências de elementos. Especifique o tipo dos elementos entre os colchetes.
-//eu posso remover(remove) ou adicionar(dd) a list
-Console.WriteLine();
-names.Add("Maria");
-names.Add("Bill");
-names.Remove("Ana");
-foreach (var name in names)
-{
-  Console.WriteLine($"Hello {name.ToUpper()}!");
-}
-////O List<T> também permite fazer referência a itens individuais por List<T>. Acesse itens usando os tokens [ e ]
-Console.WriteLine($"My name is {names[0]}.");
-Console.WriteLine($"I've added {names[2]} and {names[3]} to the list.");
-//os índices começam com 0, portanto, o maior índice válido é uma unidade a menos do que o número de itens na lista.(o item removido não aparece na contagem.)
+// See https://aka.ms/new-console-template for more information
 
-//Para verificar a quantidade de "pessoas" na list:
-Console.WriteLine($"The list has {names.Count} people in it");
+//caracteres de interpolação {}($") para melhor organização 
+//Console.WriteLine= imprime mensagem na tela 
+string aFriend = "Bill";
+Console.WriteLine($"Hello,{aFriend}");
+aFriend ="Maria";
+Console.WriteLine($"Hello {aFriend}");
+string firstFrienf = "Maria";
+string secondFriend = "Sage" ;
+Console.WriteLine($"My friends are {firstFrienf} and {secondFriend} ");
+Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
+Console.WriteLine($"The name {firstFrienf} has {firstFrienf.Length} letters.");
+string greeting = "      Hello World!       ";
+Console.WriteLine($"[{greeting}]");
 
-//Para encontrar o item na lista usa-se index ;
-var index = names.IndexOf("Felipe");
-if (index != -1)
-  Console.WriteLine($"The name {names[index]} is at index {index}");
-  //Talvez você não saiba se um item está na lista, portanto, sempre verifique o índice retornado por IndexOf. Se for -1, o item não foi encontrado.
+//CMD's TrimStart/TrimEnd/ Trim para organizar centralizando ou não//
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
 
-  // O Sort método classifica todos os itens na lista em sua ordem normal (em ordem alfabética para cadeias de caracteres).
-  names.Sort();
-foreach (var name in names)
-{
-  Console.WriteLine($"Hello {name.ToUpper()}!");
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"[{trimmedGreeting}]");
 
-  //  Para o próximo assunto tudo acima deve ser apagado para melhor entendimento.
-}
-//Isso cria uma lista de números inteiros e define os primeiros dois inteiros como o valor 1. 
-var fibonacciNumbers = new List<int> {1, 1};
-// A Sequência de Fibonacci, uma sequência de números, começa com dois 1s. Cada número Fibonacci seguinte é encontrado considerando a soma dos dois números anteriores.
-var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
-var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"[{trimmedGreeting}]");
 
-fibonacciNumbers.Add(previous + previous2);
+//ABAIXO USO DO CMD Replace para substituição com a mesma string//
+string sayHello = "Hello World!";
+Console.WriteLine(sayHello);
+sayHello = sayHello.Replace("Hello", "Greetings");
+Console.WriteLine(sayHello);
 
-foreach(var item in fibonacciNumbers)
-    Console.WriteLine(item);
-  
-  //Desafio
-  //só pega se apagar os anteriores 
-  
-var fibonacciNumbers = new List<int> {1, 1};
+//CMD's ToUpper e ToLwer deixam a letra maiuscula e menuscula respectivamente
+Console.WriteLine(sayHello.ToUpper());
+Console.WriteLine(sayHello.ToLower());
 
-while (fibonacciNumbers.Count < 20)
-{
-    var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
-    var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+//CMD Contains ,Indica se uma cadeia de caracteres contém uma subcadeia de caracteres dentro dela.Estudar mais sobre isso amanhã,ou seja, se a palavra estiver na frase - true se não -false
+string songLyrics = "You say goodbye, and I say hello";
+Console.WriteLine(songLyrics.Contains("goodbye"));
+Console.WriteLine(songLyrics.Contains("greetings"));
 
-    fibonacciNumbers.Add(previous + previous2);
-}
-foreach(var item in fibonacciNumbers)
-    Console.WriteLine(item);
+//CMD's StartsWith e EndsWith também procuram subcadeias de caracteres em uma cadeia de caracteres. Eles encontram uma subcadeia de caracteres no início ou no final da cadeia de caracteres. 
+
+string hello = "You say goodbye, and I say hello";
+Console.WriteLine(hello.StartsWith("goodbye"));//Palavra "hello" não encontrada no começo da frase - false
+Console.WriteLine(hello.EndsWith("hello"));//Palavra "hello" no final da frase - true 
+
+
